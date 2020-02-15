f60d25cf142519289cee3e803091c5bfe11f37b4
Brannen Petit <bpetit940@gmail.com>
2020-02-15 12:38:13 -0600

    completed basic functionality minus summary page and filled in results page

--- sample.js
+++ sample.js
@@ -3,7 +3,6 @@ const STORE = {
   // 5 or more questions are required
   questions: [
     {
-      currentNumber: 1, //unecessary 
       questionHead: 'Question #1',
       question: 'Question One: Which mythological realm did the God Hades reign over?',
       picture: 'https://images.squarespace-cdn.com/content/v1/58a2939a15d5dbaa30d8c8f8/1493535879754-KI6B0V9M2FIKI8TF20EC/ke17ZwdGBToddI8pDm48kFTEgwhRQcX9r3XtU0e50sUUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcW7uEhC96WQdj-SwE5EpM0lAopPba9ZX3O0oeNTVSRxdHAmtcci_6bmVLoSDQq_pb/image-asset.jpeg',
@@ -13,13 +12,12 @@ const STORE = {
         'The underworld',
         'The wilderness'
       ],
-      correctAnswer: 'The underworld'
+      correctAnswer: '3'
     },
     {
-      currentNumber: 2,
       questionHead: 'Question #2',
       question: 'Which goddess was the goddess of love in Greek mythology?',
-      picture: 'pic',
+      picture: 'https://i0.wp.com/strangeago.com/wp-content/uploads/2018/09/Venus01.jpg?resize=680%2C385&ssl=1" alt="Goddess Aphrodite being admired by her winged baby angels',
       answers: [
         'Aphrodite',
         'Freja',
@@ -29,7 +27,6 @@ const STORE = {
       correctAnswer: 'Aphrodite'
     },
     {
-      currentNumber: 3,
       questionHead: 'Question #3',
       question: 'Which greek wrote the classic epic, The Illiad?',
       picture: 'pic',
@@ -42,7 +39,6 @@ const STORE = {
       correctAnswer: 'Homer'
     },
     {
-      currentNumber: 4,
       questionHead: 'Question #4',
       question: 'Which mythological hero put Cereberus to sleep?',
       picture: 'pic',
@@ -55,7 +51,6 @@ const STORE = {
       correctAnswer: 'Orpheus'
     },
     {
-      currentNumber: 5,
       questionHead: 'Question #5',
       question: 'What is known as the "Nectar of the Gods?',
       picture: 'pic',
@@ -84,8 +79,12 @@ function renderQuestion() {
   buttonSelect();
 }
 
-function renderResults() {
-  $('.ship').html(generateResults());
+function renderCorrectResults() {
+  $('.ship').html(generateCorrectResults());
+}
+
+function renderIncorrectResults() {
+  $('.ship').html(generateIncorrectResults());
 }
 
 function generateLanding() {
@@ -127,33 +126,35 @@ function generateQuestion() {
 </div>`;
 }
 
-function generateResults() {
-  /* if () {
-    return `'<header>
+function generateCorrectResults() {
+  return `'<header>
   <h1>Correct!</h1>
   <h2>You're a genius!</h2>
 </header>
 <main><img src="https://i.pinimg.com/originals/43/2d/97/432d97353a5f6e129c7a4440f47597c0.jpg" alt="Greek comedy mask is happy for you!">
 <p></p>
-<form>
+<form id="get-results">
   <h2>You got 1/6 questions correct</h2>
   <label for="start">Keep going!</label>
   <input type="submit" id="start" value="Next Question">
 </form> 
-</main>'`
-/*else{  
-return `<header> <h1>Incorrect...</h1>
+</main>'`;
+
+}
+
+function generateIncorrectResults() { 
+  return `<header> <h1>Incorrect...</h1>
 <h2>Oops! The right answer was _______</h2>
 </header>
 <main><img src="https://spectator.imgix.net/content/uploads/2018/10/Greek-Tragedy-cover.jpg?auto=compress,enhance,format&crop=faces,entropy,edges&fit=crop&w=820&h=550" alt="Greek tradgedy set is not happy with you...">
 <p></p>
-<form>
+<form id="get-results">
 <h2>You have 0/6 questions correct</h2>
 <label for="start">Keep going!</label>
 <input type="submit" id="start" value="Next Question">
 </form> 
-</main>'`}; */
-}
+</main>'`;}
+
 
 function generateFinal() {
   //This will hold HTML for the summary
@@ -179,11 +180,23 @@ function startQuiz() {
     event.preventDefault();
     console.log('wow you guys made it this far');
     renderQuestion();
+    generateQuestion();
+    
   });
 }
 
-function nextQuestion() {
-  //This function will look for current question, and the information in STORE and place in generateQuestion, after the button is clicked
+function nextQuestionButton() {
+  //This function will look for the event click on next button on results page
+  $('#get-results').submit(event => {
+    event.preventDefault();
+    console.log('Jesus this is hard');
+    STORE.questionNumber = STORE.questionNumber + 1;
+    renderQuestion();
+    generateQuestion();
+
+  });
+  // this function will also incriment quesitonNumber by 1
+  // on click we will renderQuestion
 }
 
 function updateScore() {
@@ -194,16 +207,26 @@ function updatePage() {
   //This will increment the current page number on the STORE object.
 }
 
+function submitPage(){
+  // This function will listen for the submit action on the results page and then 
+  //render the next question
+}
+
 function buttonSelect() {
   
   //Once quiz options are chosen, this function will contain the information on what choice was selected in the question.
   $('#getResult').submit(event => {
     event.preventDefault();
+    const questionVar = STORE.questions[STORE.questionNumber];
     // const questionVar = STORE.questions[STORE.questionNumber];
     console.log($('input[type="radio"]:checked').val());
-    // if ($('input[type="radio"]:checked').val() === questionVar.correctAnswer) {
-      // generateCorrectPage()
-    // }
+    if ($('input[type="radio"]:checked').val() === questionVar.correctAnswer) {
+      renderCorrectResults();
+      nextQuestionButton();
+    } else {
+      renderIncorrectResults();
+      nextQuestionButton();
+    }
   }
   )
 }
@@ -218,8 +241,8 @@ function handleQuiz() {
   //This function calls all functions in order to run the entire quiz
   renderLanding();
   startQuiz();
-  generateQuestion();
   buttonSelect();
+  generateQuestion();
   
 }
 
